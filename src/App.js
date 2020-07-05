79d30998f12b540d0d4f6dac7768cf3d24fa2022
Waffle <cloud7717@naver.com>
2020-07-05 14:30:47 +0900

    get movie data
    get movie data using axios.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -6,11 +7,17 @@ class App extends React.Component {
     movies: []
   };
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({ movies, isLoading: false });
+  }
+
   componentDidMount() {
-    //movie data loading
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
