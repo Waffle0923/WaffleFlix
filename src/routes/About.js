6d36c54b98c78582bc0d9ede988a12e0f2217838
Waffle <cloud7717@naver.com>
2020-07-08 02:34:06 +0900

    Applying router
    Applying router and route components.

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "Without haste, but without rest."
+            </span>
+            <span>- Johann Wolfgang von Goethe -</span>
+        </div>
+    );
+}
+
+export default About;
