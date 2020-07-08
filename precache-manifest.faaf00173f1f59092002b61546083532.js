a0fc74d56b41bfe8c2786c886ded8cc714e8cf2f
Waffle <cloud7717@naver.com>
2020-07-08 14:45:25 +0900

    Updates

---
+++ precache-manifest.faaf00173f1f59092002b61546083532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add6076a2e333395e4a20d684cad8db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa671dc6bc2bea45ad33",
+    "url": "/static/css/main.227f93f4.chunk.css"
+  },
+  {
+    "revision": "11b9bb4aa10c3cb29cbd",
+    "url": "/static/js/2.73d764f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73d764f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa671dc6bc2bea45ad33",
+    "url": "/static/js/main.40839f83.chunk.js"
+  },
+  {
+    "revision": "27e4943dd2f6f69cc112",
+    "url": "/static/js/runtime-main.3c5b1439.js"
+  }
+]);
