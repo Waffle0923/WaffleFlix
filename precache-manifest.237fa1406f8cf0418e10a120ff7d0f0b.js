16b803d9384b8f86a9ec6e7f6a6ff85e9eba75c1
Waffle <cloud7717@naver.com>
2020-07-08 15:18:36 +0900

    Updates

---
+++ precache-manifest.237fa1406f8cf0418e10a120ff7d0f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e23699f83c5c96d6a6f78fa1e421468",
+    "url": "/WaffleFlix/index.html"
+  },
+  {
+    "revision": "aa671dc6bc2bea45ad33",
+    "url": "/WaffleFlix/static/css/main.227f93f4.chunk.css"
+  },
+  {
+    "revision": "11b9bb4aa10c3cb29cbd",
+    "url": "/WaffleFlix/static/js/2.73d764f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WaffleFlix/static/js/2.73d764f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa671dc6bc2bea45ad33",
+    "url": "/WaffleFlix/static/js/main.40839f83.chunk.js"
+  },
+  {
+    "revision": "082884bbb87f9cb789e9",
+    "url": "/WaffleFlix/static/js/runtime-main.82048750.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.faaf00173f1f59092002b61546083532.js"
+  "/WaffleFlix/precache-manifest.237fa1406f8cf0418e10a120ff7d0f0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WaffleFlix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
